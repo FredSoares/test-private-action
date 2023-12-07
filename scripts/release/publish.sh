cf4915ed032032d436730a82873dea178132e145
Fred Soares <29602236+FredSoares@users.noreply.github.com>
2023-12-07 16:46:07 -0100

    Release version beta 24

--- scripts/release/publish.sh
+++ scripts/release/publish.sh
@@ -10,7 +10,7 @@ NEW_VERSION=$(node -p "require('./package.json').version")
 
 # Compare versions and publish if they are different
 if [ "$OLD_VERSION" != "$NEW_VERSION" ]; then
-  echo "Version changed from $OLD_VERSION to $NEW_VERSION. Publishing to npm..."]
+  echo "Version changed from $OLD_VERSION to $NEW_VERSION. Publishing new version to npm..."]
 
   # Set authentication details
   echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> .npmrc
