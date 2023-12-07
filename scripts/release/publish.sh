b734e9656d8926a25b48908769683ef0ef97ab1e
Fred Soares <29602236+FredSoares@users.noreply.github.com>
2023-12-07 17:17:10 -0100

    Create release note

--- scripts/release/publish.sh
+++ scripts/release/publish.sh
@@ -2,31 +2,46 @@
 
 set -e
 
+# Function to log messages
+log() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@"
+}
+
 # Get version before merge
 OLD_VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
-
-# Remove 'v' prefix from VERSION if it exists
-OLD_VERSION=${OLD_VERSION#v}
+OLD_VERSION=${OLD_VERSION#v} # Remove 'v' prefix if it exists
+log "Old version: $OLD_VERSION"
 
 # Get version after merge
 NEW_VERSION=$(node -p "require('./package.json').version")
+log "New version: $NEW_VERSION"
 
 # Compare versions and publish if they are different
 if [ "$OLD_VERSION" != "$NEW_VERSION" ]; then
-  echo "Version changed from $OLD_VERSION to $NEW_VERSION. Publishing new version to npm..."
+  log "Version changed from $OLD_VERSION to $NEW_VERSION. Publishing new version to npm..."
 
   # Set authentication details
   echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> .npmrc
-  
-  # A pre-release is a version with a label i.e. v2.0.0-alpha.1
-  if [[ "$NEW_VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+-.+$ ]]
-  then
+
+  # Publish to npm
+  if [[ "$NEW_VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+-.+$ ]]; then
     yarn
     yarn publish --tag next --access=public
   else
     yarn
     yarn publish --access=public
   fi
+
+  # Generate release notes from git commits
+  RELEASE_NOTES=$(git log --pretty=format:"* %s" "$OLD_VERSION".."$NEW_VERSION")
+  echo "Release Notes for $NEW_VERSION:" > RELEASE_NOTES.md
+  echo "$RELEASE_NOTES" >> RELEASE_NOTES.md
+
+  # Create a new tag and release on GitHub
+  git tag -a "v$NEW_VERSION" -m "Release $NEW_VERSION"
+  git push origin "v$NEW_VERSION"
+  gh release create "v$NEW_VERSION" --title "Release $NEW_VERSION" --notes-file RELEASE_NOTES.md
+
 else
-  echo "Version did not change."
+  log "Version did not change."
 fi
